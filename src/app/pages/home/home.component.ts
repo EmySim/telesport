a5bdf0f3efce494daf00965571150e405df6027f
EmySim <emiliesimson@gmail.com>
2025-01-07 16:21:53 +0100

    Affichage données

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import { OlympicService } from 'src/app/core/services/olympic.service';
 import { Olympic } from '../../core/models/Olympic';  
-import { BannerComponent } from 'src/app/component/banner/banner.component';
+//import { BannerComponent } from 'src/app/component/banner/banner.component';
 
 @Component({
   selector: 'app-home',
@@ -18,7 +18,7 @@ export class HomeComponent implements OnInit {
   ngOnInit(): void {
     // Charger les données initiales
     this.olympicService.loadInitialData().subscribe((data) => {
-      this.olympics$ = this.olympicService.getOlympics();
+      this.olympics$ = this.olympicService.getOlympics(); // Récupère les données
       this.loading = false;  // Désactive l'état de chargement une fois les données chargées
     });
   }
